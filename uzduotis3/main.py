b648133fcb3493c6111c02fbd67d4d13cbb18ed1
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-19 14:13:07 +0300

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- uzduotis3/main.py
+++ uzduotis3/main.py
@@ -1,3 +1,3 @@
-pplIO = open("./ppl.txt","r",encoding="utf8")
+pplIO = open("./data.txt","r",encoding="utf8")
 linesArr = [line.rstrip('\n') for line in pplIO.readlines()[:int(pplIO.readline())]]
 print(linesArr)
