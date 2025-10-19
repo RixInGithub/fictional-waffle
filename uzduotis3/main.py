9ba7fe2ba040f88cac2e9eb28d1d79d3ddbdb591
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-19 14:11:18 +0300

    hhhh

--- uzduotis3/main.py
+++ uzduotis3/main.py
@@ -1,3 +1,3 @@
 pplIO = open("./ppl.txt","r",encoding="utf8")
-linesArr = [a[:-1] for a in [(a:=int(pplIO.readline())),pplIO.readlines()[:a]][1]]
+linesArr = [b[:-1] for b in [(a:=int(pplIO.readline())),pplIO.readlines()[:a]][1]]
 print(linesArr)
