cc9abc68c6f6f0a92aa375450145d7ffa59f3b0c
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-19 14:16:03 +0300

    finally done with the stupid workflows (perchance?)

--- uzduotis3/main.py
+++ uzduotis3/main.py
@@ -1,3 +1,3 @@
 pplIO = open("./data.txt","r",encoding="utf8")
-linesArr = [(a:=int(pplIO.readline())),[a[:-1] for a in pplIO.readlines()[:a]]]
+linesArr = [(a:=int(pplIO.readline())),[a[:-1] for a in pplIO.readlines()[:a]]][1]
 print(linesArr)
