0928a5a392c115ced5c0e13ff9d69acaa48c6514
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-19 14:30:47 +0300

    i have done it

--- uzduotis3/main.py
+++ uzduotis3/main.py
@@ -1,3 +1,10 @@
+# x%2 == 0 => dešinė (what is it in english again? fiddlesticks!!!)
+# x%2 == 1 => kairė (someone help)
+
 pplIO = open("./data.txt","r",encoding="utf8")
-linesArr = [(a:=int(pplIO.readline())),[a[:-1] for a in pplIO.readlines()[:a]]][1]
-print(linesArr)
+ppl = [(a:=int(pplIO.readline())),[list(map(int, a.split(" "))) for a in pplIO.readlines()[:a]]][1]
+pplIO.close()
+desine = []
+kaire = []
+for a in ppl: (desine if (a[0]%2==0) else kaire).append(a[1])
+print("\n".join(map(str,[(a:=sum(*desine))+(b:=sum(*kaire)), b, a, b/len(kaire), a/len(desine)])))
