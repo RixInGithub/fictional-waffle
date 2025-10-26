76469ab86ca7f0e7bc04165ca126bfd63ca3778c
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-26 13:34:10 +0200

    what

--- uzduotis10/main.py
+++ uzduotis10/main.py
@@ -6,4 +6,5 @@ with open("./data.txt","r",encoding="utf8") as lIO, open("./2encode.txt","r",enc
 	rawL = "".join([a[0] for a in letters])
 	notL = "".join([a for a in "aąbcčdeęėfghiįyjklmnoprsštuųūvzž" if not a in rawL])
 	toEncode = [reduce(lambda l, b: b.replace(l,""), notL, a) for a in dIO.read().split(" ")]
+	print(toEncode)
 	print("\n".join([" ".join([l2Coded[b] for b in a]) for a in toEncode]))
