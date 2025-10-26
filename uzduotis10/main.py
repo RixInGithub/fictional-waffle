99d42a9aeda296e1304dd9b5773d59b1cb0315c7
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-26 13:39:29 +0200

    h

--- uzduotis10/main.py
+++ uzduotis10/main.py
@@ -1,10 +1,10 @@
 from functools import reduce
 with open("./data.txt","r",encoding="utf8") as lIO, open("./2encode.txt","r",encoding="utf8") as dIO:
 	letters = int(lIO.readline())
-	letters = [a.split(" ") for a in lIO.readlines()[:letters]]
+	letters = [a.rstrip("\n").split(" ") for a in lIO.readlines()[:letters]]
 	l2Coded = dict(letters) # entries => dict (for ezier management)
 	rawL = "".join([a[0] for a in letters])
 	notL = "".join([a for a in "aąbcčdeęėfghiįyjklmnoprsštuųūvzž" if not a in rawL])
 	toEncode = dIO.read().split(" ")
 	print(toEncode, l2Coded)
-	print("\n".join([" ".join([l2Coded[b] for b in a if l2Coded.get(a,None)!=None]) for a in toEncode]))
+	print("\n".join([" ".join([l2Coded[b] for b in a if l2Coded.get(b,None)!=None]) for a in toEncode]))
