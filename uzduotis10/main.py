27639ed0c1370f26335e440d2ad9ea61698de483
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-10-26 13:37:55 +0200

    why no work

--- uzduotis10/main.py
+++ uzduotis10/main.py
@@ -6,5 +6,5 @@ with open("./data.txt","r",encoding="utf8") as lIO, open("./2encode.txt","r",enc
 	rawL = "".join([a[0] for a in letters])
 	notL = "".join([a for a in "aąbcčdeęėfghiįyjklmnoprsštuųūvzž" if not a in rawL])
 	toEncode = dIO.read().split(" ")
-	print(toEncode)
+	print(toEncode, l2Coded)
 	print("\n".join([" ".join([l2Coded[b] for b in a if l2Coded.get(a,None)!=None]) for a in toEncode]))
