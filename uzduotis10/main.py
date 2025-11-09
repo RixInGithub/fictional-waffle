07909209d0aa6696876aa73b3d95a3d4efa3926e
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-11-09 14:20:33 +0200

    minor fixes

--- uzduotis10/main.py
+++ uzduotis10/main.py
@@ -1,4 +1,3 @@
-from functools import reduce
 with open("./data.txt","r",encoding="utf8") as lIO, open("./2encode.txt","r",encoding="utf8") as dIO:
 	letters = int(lIO.readline())
 	letters = [a.rstrip("\n").split(" ") for a in lIO.readlines()[:letters]]
