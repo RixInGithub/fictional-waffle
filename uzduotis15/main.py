801110f6df97e2b4849bb7b8d815d35485dace7a
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-11-09 13:03:59 +0200

    h?

--- uzduotis15/main.py
+++ uzduotis15/main.py
@@ -10,9 +10,8 @@ if not exists:
 	with open("saveData.json","w",encoding="utf8") as startIO: json.dump([nm,logs,rox],startIO)
 # look man, i genuinely have no excuse as to why am i doing this. i just suck at making games bruh. i just want good grade man
 with open("saveData.json","r+",encoding="utf8") as dataIO:
-	nm,logs,rox = dataIO.read()
+	nm,logs,rox = json.load(dataIO)
 	dataIO.seek(0)
-	dataIO.truncate(0)
 	running = True
 	while running:
 		print("0 - exit\n1 - keisti vardą\n2 - kapoti medžio\n3 - kasti akmens\n4 - rodyti medžio+akemns turį")
@@ -29,3 +28,6 @@ with open("saveData.json","r+",encoding="utf8") as dataIO:
 				print(f"{logs} med{"žiai" if logs!=1 else "is"}\n{rox} akmen{"y" if logs!=1 else "i"}s")
 			case _: # if opt is 0 or smth invalid
 				running = False
+	# when we exit, we need to save
+	dataIO.seek(0)
+	json.dump([nm,logs,rox],dataIO)
