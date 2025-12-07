5a1ef7a7641b6a5bad63b4861d10e6142538c6ba
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-12-07 13:38:33 +0200

    idk

--- uzduotis20/main.py
+++ uzduotis20/main.py
@@ -4,16 +4,13 @@ class Animal:
 		print("...")
 
 class Dog(Animal):
-	def makeSound(self):
-		print("woof")
+	def makeSound(self): print("woof")
 
 class Cat(Animal):
-	def makeSound(self):
-		print("meoww")
+	def makeSound(self): print("meoww")
 
 class Cow(Animal):
-	def makeSound(self):
-		print("moooo")
+	def makeSound(self): print("moooo")
 
 def playSound(a):
 	a.makeSound()
