0accd10563ac658e36caf0512c4b7bb89be95af1
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-12-07 12:49:48 +0200

    duck

--- uzduotis19/main.py
+++ uzduotis19/main.py
@@ -30,7 +30,7 @@ with open("data.txt", "r", encoding="utf8") as pplIO:
 	ppl = pplIO.read().split("\n")
 	for p in ppl:
 		pList = p.split(" ")
-		pCls = globals()[pList[2]]
+		pCls = globals()[pList[3]]
 		pRest = pList[4:]
 		if pList[2] == "Builder": pRest = [Resource(*r.split("-")) for r in pRest]
 		pObj = pCls(pList[0], pList[1], pList[2], *pList[4:])
